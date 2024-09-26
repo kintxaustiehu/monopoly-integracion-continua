f87bd9cccfa30f655d66425aa8cc95a0bad10945
koldointxausti <koldointxaustii@gmail.com>
2024-09-26 16:06:19 +0200

    Prueba

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -1,5 +1,5 @@
 package edu.ncsu.monopoly;
-//I have added a comment“
+//I have added a commentï¿½
 public abstract class Card {
 
     public static final int TYPE_CHANCE = 1;
@@ -8,4 +8,5 @@ public abstract class Card {
     public abstract String getLabel();
     public abstract void applyAction();
     public abstract int getCardType();
+
 }
